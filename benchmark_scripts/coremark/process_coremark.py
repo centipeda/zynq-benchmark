670962a2b15c0d377db7dc6f83e83379345c39e1
centipeda <joshua.c.guam@gmail.com>
2020-06-17 19:51:08 +1000

    remove old script files

--- benchmark_scripts/coremark/process_coremark.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import statistics as stats
-
-with open('results.txt') as resultFile:
-    runs = [float(l.split(' ')[3]) for l in resultFile.readlines()]
-    print("Run results: {}".format(runs))
-    print("Mean: {}".format(stats.mean(runs)))
-    print("Standard deviation: {}".format(stats.stdev(runs)))
--- benchmark_scripts/process_whetstone.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import statistics as stats
-
-with open('results.txt') as resultFile:
-    runs = [float(l.split(' ')[-2]) for l in f.readlines()]
-    print("Run results: {}".format(runs))
-    print("Mean: {}".format(stats.mean(runs)))
-    print("Standard deviation: {}".format(stats.stdev(runs)))
