fb6ae19213fc9e801e35a219955479b376e8bbd3
centipeda <joshua.c.guam@gmail.com>
2020-06-17 19:52:28 +1000

    remove unnecessary script file

--- process_hw.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import statistics as stats
-
-with open('ps.txt', 'r') as f:
-    runs = [float(line.strip()) for line in f]
-    print("Run results: {}".format(runs))
-    print("Mean: {}".format(stats.mean(runs)))
-    print("Standard deviation: {}".format(stats.stdev(runs)))
