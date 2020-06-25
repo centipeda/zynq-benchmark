96663a7dd6b4060cea27d2c96b404b24d9d84303
centipeda <joshua.c.guam@gmail.com>
2020-06-25 18:16:37 +1000

    Add script to generate HTML table
    Adds script in docs to generate HTML table for auto-regenerating
    benchmark score table.

---
+++ docs/generate_table.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import os
+import glob
+import sys
+import yaml
+from collections import OrderedDict
+
+RESULTS_DIR = "../benchmark_results"
+HEADERS = OrderedDict((
+    ("coremark", "Coremark"),
+    ("whetstone", "Whetstone"),
+    ("dhrystone_with_registers", "Dhrystone, with registers"),
+    ("dhrystone_without_registers", "Dhrystone, without registers")
+))
+
+print("<table>")
+print("  <tr>")
+print("<th>Platform</th>")
+for header in HEADERS:
+    print(f"    <th>{HEADERS[header]}</th>")
+print("  </tr>")
+
+for resultDir in os.listdir(RESULTS_DIR):
+    summaryFiles = glob.glob(os.path.join(RESULTS_DIR, resultDir, '*_summary.txt'))
+    if summaryFiles:
+        with open(summaryFiles[0]) as sFile:
+            print("  <tr>")
+            print(f"<td>{summaryFiles[0]}</td>")
+            scores = yaml.load(sFile, Loader=yaml.BaseLoader)
+            for header in HEADERS:
+                print(f"    <td>{scores[header]['mean']}</td>")
+            print("  </tr>")
+
+    else:
+        print(f'No summary file found in {os.path.join(RESULTS_DIR, resultDir, "*_summary.txt")}')
+
+print("</table>")
