04ff9e6600cbee8792bb0cab244531bcdf2db1a0
centipeda <joshua.c.guam@gmail.com>
2020-07-07 18:28:02 +1000

    Add extra information to table generator
    The table generator will now look for an extra file in each results
    directory named 'information.txt', which will be loaded as a YAML file,
    that includes extra information about the run that might be more easily
    provided by the user than automatically detected, such as specific
    copmiler version, CPU name, platform name, and run date/time.

--- docs/generate_table.py
+++ docs/generate_table.py
@@ -5,33 +5,49 @@ import sys
 import yaml
 from collections import OrderedDict
 
-RESULTS_DIR = sys.argv[1]
+RESULTS_BASE_DIR = sys.argv[1]
 HEADERS = OrderedDict((
     ("coremark", "Coremark"),
     ("whetstone", "Whetstone"),
     ("dhrystone_with_registers", "Dhrystone, with registers"),
     ("dhrystone_without_registers", "Dhrystone, without registers")
 ))
+INFO_HEADERS = OrderedDict((
+    ("name", "Platform"),
+    ("compiler", "Compiler"),
+    ("cpu", "CPU"),
+    ("date", "Date"),
+    ("notes", "Notes"),
+))
 
 print("<table>")
 print("  <tr>")
-print("<th>Platform</th>")
+for header in INFO_HEADERS:
+    print(f"    <th>{INFO_HEADERS[header]}</th>")
 for header in HEADERS:
     print(f"    <th>{HEADERS[header]}</th>")
 print("  </tr>")
 
-for resultDir in os.listdir(RESULTS_DIR):
-    summaryFiles = glob.glob(os.path.join(RESULTS_DIR, resultDir, '*_summary.txt'))
+for resultDir in os.listdir(RESULTS_BASE_DIR):
+    infoFile = 'information.txt'
+
+    print("  <tr>")
+    try:
+        with open(os.path.join(RESULTS_BASE_DIR, resultDir, infoFile), 'r') as info:
+            information = yaml.load(info, Loader=yaml.BaseLoader)
+            for header in INFO_HEADERS:
+                print(f"    <td>{information[header]}</td>")
+    except FileNotFoundError:
+        print(f'No information file found in {os.path.join(RESULTS_BASE_DIR, resultDir, infoFile)}')
+
+    summaryFiles = glob.glob(os.path.join(RESULTS_BASE_DIR, resultDir, '*_summary.txt'))
     if summaryFiles:
         with open(summaryFiles[0]) as sFile:
-            print("  <tr>")
-            print(f"<td>{resultDir}</td>")
             scores = yaml.load(sFile, Loader=yaml.BaseLoader)
             for header in HEADERS:
                 print(f"    <td>{scores[header]['mean']}</td>")
-            print("  </tr>")
-
     else:
-        print(f'No summary file found in {os.path.join(RESULTS_DIR, resultDir, "*_summary.txt")}')
+        print(f'No summary file found in {os.path.join(RESULTS_BASE_DIR, resultDir, "*_summary.txt")}')
+    print("  </tr>")
 
 print("</table>")
